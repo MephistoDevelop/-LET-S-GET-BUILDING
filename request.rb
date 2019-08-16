f62bdce45cbfc755010e79cbd6dd2775347c521c
serdg0 <sergio.diazg92@gmail.com>
2019-08-16 13:55:11 -0500

    Executed rubocop

--- request.rb
+++ request.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 
-  
 puts 'Type your Bing search'
 search = gets.chomp
-response = RestClient.get"https://www.bing.com/search?q='#{search}'"
+response = RestClient.get "https://www.bing.com/search?q='#{search}'"
 puts response.code
-puts '-'*20
+puts '-' * 20
 puts 'Header:'
 puts response.headers
-puts '-'*20
+puts '-' * 20
 puts 'Body:'
 puts response.body
-puts '-'*20
+puts '-' * 20
 puts 'Cookies:'
 puts response.cookies
