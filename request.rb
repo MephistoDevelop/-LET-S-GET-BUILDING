6dc2f8d6bbcf9f8cf0bf1f4773f92cadf275a79a
serdg0 <sergio.diazg92@gmail.com>
2019-08-16 13:39:33 -0500

    Bing requests done

--- request.rb
+++ request.rb
@@ -1,7 +1,16 @@
 require 'rest-client'
 
-def request
-  return "Hello Web Request !!"
-end
-
-puts request()
+  
+puts 'Type your Bing search'
+search = gets.chomp
+response = RestClient.get"https://www.bing.com/search?q='#{search}'"
+puts response.code
+puts '-'*20
+puts 'Header:'
+puts response.headers
+puts '-'*20
+puts 'Body:'
+puts response.body
+puts '-'*20
+puts 'Cookies:'
+puts response.cookies
